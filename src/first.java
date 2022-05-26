308e87bea1b1107cff5e34a586a6f880250a11e3
Girych <dmitriev.ilya@mail.ru>
2022-05-26 19:51:04 +0300

    commit

---
+++ src/first.java
@@ -0,0 +1,10 @@
+public class first {
+
+    public static void main(String[] args) {
+
+        int ticket = 23766;
+        int mile = 20;
+        int bonus = ticket / mile;
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
